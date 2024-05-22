7476bacb65a21fa99baada499d0d468e11f69d08
Frank Williames <dev.frankw@gmail.com>
2024-05-21 23:54:17 -0400

    adicionando assests e classe GUI

---
+++ classes/Gui.lua
@@ -0,0 +1,38 @@
+local class = require("library/middleclass")
+local Button = require("classes/Button")
+
+---@class Gui
+local Gui = class("gui")
+
+function Gui:initialize()
+    
+    self.quitButton =       Button:new("QuitText")
+    self.holderBar =        Button:new("HealthEnergyHolder")
+    self.healthBarHurt =    Button:new("HealthBarHurt2")
+    self.healthBarHealthy = Button:new("HealthBarHealthy1")
+    self.energyEmpty =      Button:new("EnergyEmpty2")
+    self.energyFull =       Button:new("EnergyFull1")
+
+end
+
+function Gui:draw()
+    
+    self.quitButton:draw()
+    self.holderBar:draw()
+    self.healthBarHurt:draw()
+    self.healthBarHealthy:draw()
+    self.energyEmpty:draw()
+    self.energyFull:draw()
+    
+end
+
+function Gui:mousepressed(x, y, button, istouch, presses)
+    if button == 1 then -- botão esquerdo do mouse
+        if x >= self.quitButton.hitBoxX[1] and x <= self.quitButton.hitBoxX[2] and
+            y >= self.quitButton.hitBoxY[1] and y <= self.quitButton.hitBoxY[2] then
+            love.event.quit() -- fechar o jogo
+        end
+    end
+end
+
+return Gui
