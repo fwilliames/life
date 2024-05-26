ab9a8ec0c43a1377c2a6b00890480622bbba44cf
Frank Williames <dev.frankw@gmail.com>
2024-05-26 16:26:35 -0400

    GAME-16 Adicionado ajusta de escala da classe Spell
    Adicionado ajuste de escala dos elementos de spell quando a janela for redimensionada

--- classes/Spell.lua
+++ classes/Spell.lua
@@ -9,23 +9,24 @@ function Spell:initialize(name,x,y)
     self.y = y + 130
     self.radio = 180 --180
     self.useSpell = false
-    self.hitBoxes = {{ self.x + 15, self.x + 2 * self.radio}, {self.y + 15, self.y + self.radio*2}}
+    self.hitBoxes = {{ (self.x + 15) * widthCorrectionFactor, (self.x + 2 * self.radio) * widthCorrectionFactor}, {(self.y + 15) * heightCorrectionFactor, (self.y + self.radio * 2) * heightCorrectionFactor}}
     self.isPlayer = false
     self.mana = 10
 end                      
 
 function Spell:draw()                        
     if self.useSpell then
-        --love.graphics.rectangle("fill", self.x + 15, self.y + 15, self.radio * 2, self.radio*2)
+        --love.graphics.rectangle("fill", self.hitBoxes[1][1], self.hitBoxes[2][1], self.radio * 2 * widthCorrectionFactor, self.radio * 2 * heightCorrectionFactor)
         --love.graphics.circle("fill", 285 + self.x - 90, 285 +  self.y - 90, self.radio)
-        love.graphics.draw(self.image, self.x, self.y, 0, 0.7, 0.7) --width[1] - 90, heigh[1] - 50,0,1,1)
+        love.graphics.draw(self.image, self.x * widthCorrectionFactor, self.y * heightCorrectionFactor, 0, 0.7 * widthCorrectionFactor, 0.7 * heightCorrectionFactor) --width[1] - 90, heigh[1] - 50,0,1,1)
     end
 end
 
 function Spell:update(x,y)
     self.x = x + 60
     self.y = y + 130
-    self.hitBoxes = {{ self.x + 15, self.x + 2 * self.radio}, {self.y + 15, self.y + self.radio*2}}
+    self.hitBoxes = {{ (self.x + 15) * widthCorrectionFactor, (self.x + 2 * self.radio) * widthCorrectionFactor}, {(self.y + 15) * heightCorrectionFactor, (self.y + self.radio * 2) * heightCorrectionFactor}}
+
 
 end
 
